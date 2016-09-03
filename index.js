751a2bd0f40c03153d3cca6ed8a412b3fb8d5232
Cristian Pirvan <pirvan.cristiani@yahoo.com>
2016-09-03 19:22:18 +0200

    test

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+
 var express = require('express')
 
 var app = express()
 
+var test = ''
+
 app.get('/notes', function(req, res) {
   res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
+})
+
+app.get('/users', function(req, res) {
+  res.json({users: "The GET for the users!"})
 })
 
 app.listen(3000)
