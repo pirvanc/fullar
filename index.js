48ac383dcc79550a28d8586f927c3b6ff892a748
Cristian Pirvan <pirvan.cristiani@yahoo.com>
2016-09-03 20:42:27 +0200

    HelloWorld

--- index.js
+++ index.js
@@ -5,12 +5,8 @@ var app = express()
 
 var test = ''
 
-app.get('/notes', function(req, res) {
-  res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
-})
-
-app.get('/users', function(req, res) {
-  res.json({users: "The GET for the users!"})
+app.get('/Helloworls', function(req, res) {
+  res.json({notes: "Hello World!"})
 })
 
 app.listen(3000)
